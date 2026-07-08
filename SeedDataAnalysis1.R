cae28c3f7529c9a6af313f1c9f24381d2f6b8c1f
IsopodFan <jack.r.brownfield@gmail.com>
2026-07-08 15:52:41 -0400

    goofin

--- SeedDataAnalysis1.R
+++ SeedDataAnalysis1.R
@@ -49,6 +49,11 @@
   #Import data
     WideCounts <- read_excel(here("All_Data_Seeds-7_26.xlsx"), sheet = "COUNTS")
     WideWeights <- read_excel(here("All_Data_Seeds-7_26.xlsx"), sheet = "WEIGHTS")
+    
+  #Add column combining Site and Quad
+    WideCounts$SiteQuad <- paste(WideCounts$Site, WideCounts$Quad, sep = "_")
+    WideCounts <- WideCounts |> 
+      relocate(SiteQuad, .before = Site)
   #Flip data
     LongCounts <- gather(data = WideCounts, 
                          key = "Species", 
@@ -67,6 +72,7 @@
     
   #don't want: unks 6, 10, 12, 13, 14, 16, 17, 19, 20, 21, 22, 24, 25, 26, 28
   #remove unknowns
+    WideCounts <- WideCounts[, !grepl("Unknown|Uknown|Notes|134", names(WideCounts))]
     LongCounts <- LongCounts |> 
         filter(!str_detect(LongCounts$Species, "Unknown")) |> 
         #remove uknowns
@@ -85,4 +91,33 @@ All_Bar <- ggplot(Bar_Data, aes(x = Species, y = n)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   scale_y_log10()
 
+#diversity stuff
+WCna <- WideCounts |> 
+  mutate(across(12:107, as.numeric)) |> 
+  #use mutate across everything to turn all instances of "NA" into a 0
+  mutate(across(c(Acer_platanoides_count:Helianthus_annus_count), ~replace_na(., 0))) 
+  #convert all data columns into numeric 
 
+div <- diversity(WCna[,12:107], index = "shannon")
+even <- div/log(specnumber(WCna[,12:107]))
+
+WCna$div <- div
+WCna$even <- even
+
+WCna.div <- WCna |> 
+  group_by(Set, SiteQuad) |> 
+  summarise(
+    diversity = mean(div),
+    .groups = "drop"
+  )
+
+WC.div.avg <- WCna.div |> 
+  group_by(Set) |> 
+  summarize(
+    avg_div = mean(diversity),
+    .groups = "drop"
+  )
+
+ggplot(WC.div.avg, aes(x = Set, y = avg_div)) +
+  geom_point() +
+  geom_line()
